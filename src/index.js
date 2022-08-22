59e1c279de7edd0bb175f1f019f3b3caf173a723
Angelo Huaraca <ahuaracab@gmail.com>
2022-08-22 01:17:17 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import {
+    dirname,
+    join
+} from 'path'
+import {
+    fileURLToPath
+} from 'url'
+//import morgan from 'morgan'
+
+//requiring routes
+import indexRoutes from './routes/index.js'
+
+const app = express();
+const __dirname = dirname(fileURLToPath(
+    import.meta.url))
+
+//settings
+app.set('appName', 'Mi primer server')
+app.set('views', join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+//middlewares
+//app.use(morgan('dev'))
+
+/*app.use((req, res, next) => {
+    console.log('request url: ' + req.url)
+    next()
+})*/
+
+//rutas
+app.use(indexRoutes)
+app.use(express.static(join(__dirname, 'public')))
+
+app.get('/*', (req, res) => {
+    res.end('Archivo no encontrado!')
+});
+
+app.listen(3000, () => {
+    console.log('Servidor funcionando!')
+    console.log('Nombre de la app: ', app.get('appName'))
+});
---
+++ src/routes/index.js
@@ -0,0 +1,25 @@
+import {
+    Router
+} from 'express'
+
+const router = Router()
+
+router.get('/', (req, res) => {
+    res.render('index', {
+        title: 'First Website with Node'
+    })
+})
+
+router.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me'
+    })
+})
+
+router.get('/contact', (req, res) => {
+    res.render('contact', {
+        title: 'Contact Page'
+    })
+})
+
+export default router
